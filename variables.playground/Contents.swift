3053ac22e62aa35e98e664353e15d01be0015a0e
Jan r3p <janr3p@Jans-Mac-mini.local>
2023-11-24 12:46:29 +0000

    adding swift file for learning variables

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground" // using 'var' to declare variables means that the variable is mutable
+
+print(greeting) // "Hello, playground"
+
+greeting = "Hello, again" // assigning new value to the variable
+
+print(greeting) // "Hello, again"
